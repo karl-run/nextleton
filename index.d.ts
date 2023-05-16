c4433cbdb50925a8d82195db2803664b8da39db1
Karl J. Overå <k@rl.run>
2023-05-16 14:57:16 +0200

    fix typing on LazyNextleton morerer bettererer

--- index.d.ts
+++ index.d.ts
@@ -3,13 +3,10 @@ export declare function nextleton<Key extends string, Result>(
   fn: () => Result
 ): Result;
 
-declare interface LazyNextleton<Result> {
-  (fn: () => Result): () => Result;
-
-  reset: () => void;
-}
-
 export declare function lazyNextleton<Key extends string, Result>(
   key: Key,
   fn: () => Result
-): LazyNextleton<Result>;
+): {
+  (): Result;
+  reset(): void;
+};
