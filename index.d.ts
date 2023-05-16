105a8e82c0fe7f10a903e50e456865178408a56b
Karl J. Overå <k@rl.run>
2023-05-16 13:46:03 +0200

    add lazy singleton

--- index.d.ts
+++ index.d.ts
@@ -2,3 +2,8 @@ export declare function nextleton<Key extends string, Result>(
   key: Key,
   fn: () => Result
 ): Result;
+
+export declare function lazyNextleton<Key extends string, Result>(
+  key: Key,
+  fn: () => Result
+): () => Result;
--- index.mjs
+++ index.mjs
@@ -5,3 +5,12 @@ export function nextleton(key, fn) {
     return glocal[key];
   })(global);
 }
+
+export function lazyNextleton(key, fn) {
+  return () =>
+    ((glocal) => {
+      glocal[key] = glocal[key] ?? fn();
+
+      return glocal[key];
+    })(global);
+}
